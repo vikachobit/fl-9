6531e6fd2503871c0285cca914980a5c465e7655
unknown <vika.chobit23@gmail.com>
2018-08-21 00:01:59 +0300

    homework7:ready and validated

--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -8,49 +8,52 @@ if (!offer) {
 } else {
     startGame(MAX_PRIZE, AMOUNT, totalPrize)
 }
+
 /*eslint no-magic-numbers: ["error", { "ignore": [0,1,2,3,4] }]*/
 function startGame(maxPrize, range, totalSum) {
     let randomNum = Math.floor(Math.random() * (range + 1));
     let currentPrize = maxPrize;
     for (let i = 0; i <= 2; i++) {
-        let userNum = parseFloat(prompt(`Enter a number in range [0 ; ${range}]` + `\n` +
-            `Attempts left: ${3 - i}` + `\n` +
-            `Total prize: ${totalSum}$` + `\n` +
+        let userNum = parseFloat(prompt(`Enter a number in range [0 ; ${range}]\n`+
+            `Attempts left: ${3 - i}\n`+
+            `Total prize: ${totalSum}$\n`+
             `Maximum possible prize in this game: ${currentPrize}$`));
         if (randomNum === userNum) {
             switch (i) {
                 case 0:
                     totalSum += currentPrize;
-                    confirm(`Congratulation! Your price is ${currentPrize}$` + `\n` +
+                    confirm(`Congratulation! Your price is ${currentPrize}$.\n`+
                         `Do you want to continue?`) ? startGame(maxPrize * 3, range * 2, totalSum) : endGame(totalSum);
                     break;
                 case 1:
                     currentPrize = maxPrize / 2;
                     totalSum += currentPrize;
-                    confirm(`Congratulation! Your price is ${currentPrize}$`+`\n`+
-                    `Do you want to continue?`) ? startGame(maxPrize * 3, range * 2, totalSum) : endGame(totalSum);
+                    confirm(`Congratulation! Your price is ${currentPrize}$.\n`+
+                        `Do you want to continue?`) ? startGame(maxPrize * 3, range * 2, totalSum) : endGame(totalSum);
                     break;
                 case 2:
                     currentPrize = Math.floor(maxPrize / 4);
                     totalSum += currentPrize;
-                    confirm(`Congratulation! Your price is ${currentPrize}$`+`\n`+
-                    `Do you want to continue?`) ? startGame(maxPrize * 3, range * 2, totalSum) : endGame(totalSum);
+                    confirm(`Congratulation! Your price is ${currentPrize}$\n`+
+                        `Do you want to continue?`) ? startGame(maxPrize * 3, range * 2, totalSum) : endGame(totalSum);
                     break;
-                default: break;
+                default:
+                    break;
             }
             break;
         } else {
             switch (i) {
-                case 1:
+                case 0:
                     currentPrize = maxPrize / 2;
                     break;
-                case 2:
+                case 1:
                     currentPrize = Math.floor(maxPrize / 4);
                     break;
-                case 3:
+                case 2:
                     endGame(totalPrize);
                     break;
-                default: break;
+                default:
+                    break;
             }
         }
     }
