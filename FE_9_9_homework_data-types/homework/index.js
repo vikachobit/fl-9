549b1cc98c2fb3be80b729c928db1a61747a8a86
unknown <vika.chobit23@gmail.com>
2018-08-24 22:35:17 +0300

    filter, getAdultAppleLovers

--- FE_9_9_homework_data-types/homework/index.js
+++ FE_9_9_homework_data-types/homework/index.js
@@ -9,19 +9,83 @@ function forEach(arr, callback) {
 }
 
 function map(arr, callback) {
-    let newArr = [];
-    forEach(arr, (item) => newArr.push(callback(item)));
-    console.log(newArr);
+    let modifyArr = [];
+    forEach(arr, (item) => {
+        modifyArr.push(callback(item))
+    });
+    console.log(modifyArr);
+    return modifyArr;
+
 }
+
+function filter(arr, callback) {
+    let filteredArray = [];
+    forEach(arr, (item) => {
+        if (callback(item)) {
+            filteredArray.push(item)
+        }
+    });
+    console.log(filteredArray);
+    return filteredArray;
+}
+
+function getAdultAppleLovers(arr) {
+    let filteredArray = filter(arr, item => {
+        return item.age > 18 && item.favoriteFruit === "apple"
+    });
+    map(filteredArray, (item) => {
+        return item.name
+    })
+}
+
+let data = [
+    {
+        "_id": "5b5e3168c6bf40f2c1235cd6",
+        "index": 0,
+        "age": 39,
+        "eyeColor": "green",
+        "name": "Stein",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5b5e3168e328c0d72e4f27d8",
+        "index": 1,
+        "age": 38,
+        "eyeColor": "blue",
+        "name": "Cortez",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5b5e3168cc79132b631c666a",
+        "index": 2,
+        "age": 2,
+        "eyeColor": "blue",
+        "name": "Suzette",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5b5e31682093adcc6cd0dde5",
+        "index": 3,
+        "age": 17,
+        "eyeColor": "green",
+        "name": "Weiss",
+        "favoriteFruit": "banana"
+    }
+];
 
 
 //call
 console.log(findType(null));
 
-forEach([2, 5, 10], function (el) {
+forEach([2, 5, 8], function (el) {
     console.log(el)
 });
 
-map([2, 5, 10], function (el) {
+map([2, 5, 8], function (el) {
     return el + 3
 });
+filter([2, 5, 8], function (el) {
+    return el > 3
+});
+
+getAdultAppleLovers(data); // returns [‘Stein’]
